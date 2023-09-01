6fd567068833880ee5824f6b767aaf0b3ce9decf
otavioooo <129073800+otavioooo@users.noreply.github.com>
2023-09-01 09:56:02 -0300

    Add files via upload

---
+++ Contagem Regressiva.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+print('</2023>')
+
+sleep(1)
+print('CONTAGEM REGRESSIVA PARA 2024')
+
+for cont in range(10, -1, -1):
+
+    print(cont)
+
+    sleep(1)
+
+print('Feliz ano novo!')
+print('<2023>')
+print('Fogo nos Petistas!',end='\n\n')
